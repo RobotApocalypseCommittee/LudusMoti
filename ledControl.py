3cf22129883b85166ea0628041782e3aa1d15bc9
ObjectionTheory <therandomguy265@gmail.com>
2017-03-17 10:59:22 +0000

    Led Controller
    Manages Leds from GPIO pins

--- ledControl.py
+++ ledControl.py
@@ -13,31 +13,29 @@ class Led:
         #setup led as output
         GPIO.setup(self.pinNo, GPIO.OUT)
         
-        #set default state as off
-        self.state = False
-
-        self.blinking = False
-
-        self.happy = True
+        #set default states as off
+        self.off()
         
-
+    #Manually change led states
     def set_led(self, state):
         self.state = state
         GPIO.output(self.pinNo, state)
 
+    #Toggle Leds
     def toggle_led(self):
         state = not self.state
         GPIO.output(ledno, state)
 
+    #Blink function; needs to implemented in main
     def blink(self):
         if blinking:
             self.toggle_led()
         else:
             self.blinking = True
 
-    def kill(self):
+    #Set all states to 0
+    def off(self):
         self.state = False
         self.blinking = False
-
     
     
