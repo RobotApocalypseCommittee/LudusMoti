d881b0f4693d363e9d7b035f30313a927bf733fd
ObjectionTheory <therandomguy265@gmail.com>
2017-03-17 10:36:37 +0000

    Led Control
    Class for individual led control using RPI.GPIO

--- ledControl.py
+++ ledControl.py
@@ -1,25 +1,43 @@
 #Import module
 import RPi.GPIO as GPIO
 
-class LedDriver:
-    def __init__(self):
-        #set the board mode as standard (just ignore this)
-        GPIO.setmode(GPIO.BCM)
+#set the board mode as standard (just ignore this)
+GPIO.setmode(GPIO.BCM)
 
-        chanList = [21, 26]
+#Individual leds become new class instances
+class Led:
+    def __init__(self, pin):
 
-        #setup all the pins in the array as output pins
-        GPIO.setup(chanList, GPIO.OUT)
+        self.pinNo = pin
+        
+        #setup led as output
+        GPIO.setup(self.pinNo, GPIO.OUT)
+        
+        #set default state as off
+        self.state = False
 
-        self.leds = {
-            21: False,
-            26: False
-        }
+        self.blinking = False
 
-    def set_led(self, ledno, state):
-        self.leds[ledno] = state
+        self.happy = True
+        
+
+    def set_led(self, state):
+        self.state = state
+        GPIO.output(self.pinNo, state)
+
+    def toggle_led(self):
+        state = not self.state
         GPIO.output(ledno, state)
 
-    def toggle_led(self, ledno):
-        state = self.leds[ledno] = not self.leds[ledno]
-        GPIO.output(ledno, state)
+    def blink(self):
+        if blinking:
+            self.toggle_led()
+        else:
+            self.blinking = True
+
+    def kill(self):
+        self.state = False
+        self.blinking = False
+
+    
+    
